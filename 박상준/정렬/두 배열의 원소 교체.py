47598ef65ba823347b031eff960960f3c1d7aa80
jihye94 <qkrtkdwns3410@naver.com>
2022-07-19 14:22:29 +0900

    정렬 - 두 배열의 원소 교체

--- 박상준/정렬/두 배열의 원소 교체.py
+++ 박상준/정렬/두 배열의 원소 교체.py
@@ -9,3 +9,18 @@
  * -----------------------------------------------------------
  * 2022-07-19        jihye94       최초 생성
  """
+import sys
+
+input = sys.stdin.readline
+
+# n = 배열의 길이 || k  = 바꿔치기 가능한 횟수
+n, k = map(int, input().split())
+
+a = sorted(list(map(int, input().split())))
+b = sorted(list(map(int, input().split())), reverse=True)
+
+for i in range(k):
+    if a[i] < b[i]:
+        a[i], b[i] = b[i], a[i]
+
+print(sum(a))
