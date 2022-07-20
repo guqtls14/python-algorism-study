be2c78c30a205d8d3db417a0e096b0508a542c58
jihye94 <qkrtkdwns3410@naver.com>
2022-07-20 15:12:00 +0900

    정렬 - 통계학

---
+++ 박상준/정렬/2108_백준_통계학.py
@@ -0,0 +1,42 @@
+"""
+ *packageName    : 
+ * fileName       : 2108_통계학
+ * author         : jihye94
+ * date           : 2022-07-20
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-07-20        jihye94       최초 생성
+ """
+import sys
+
+input = sys.stdin.readline
+# n 홀수
+import collections
+
+n = int(input())
+n_arr = list(int(input()) for _ in range(n))
+
+# 오름차순 정렬
+n_arr.sort()
+
+# 산술평균
+average_num = round(sum(n_arr) / n)
+print(average_num)
+
+# 중앙값
+median_num = n_arr[n // 2]
+print(median_num)
+
+count = collections.Counter(n_arr)
+# 최빈값
+if n > 1:
+    counter_num = sorted(count.most_common(n=2), key=lambda x: (x[1], x[0]))
+    print(counter_num.pop()[0])
+else:
+    print(count.most_common(n=1).pop()[0])
+
+# 범위 출력
+range_num = abs(max(n_arr) - min(n_arr))
+print(range_num)
