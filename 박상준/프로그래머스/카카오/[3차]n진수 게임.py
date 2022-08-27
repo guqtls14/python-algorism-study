12442e79c12808c61a3af8444ad387ceaf5ef19e
jihye94 <qkrtkdwns3410@naver.com>
2022-08-27 18:20:16 +0900

    구현

---
+++ 박상준/프로그래머스/카카오/[3차]n진수 게임.py
@@ -0,0 +1,27 @@
+"""
+ *packageName    : 
+ * fileName       : [3차]n진수 게임
+ * author         : qkrtkdwns3410
+ * date           : 2022-08-27
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-08-27        qkrtkdwns3410       최초 생성
+ """
+
+# 진법 n || 미리 구할 숫자의 갯수 t || 게임에 참가하는 인원 m || 튜브의 순서 p ||
+from collections import defaultdict
+
+
+def solution(n, t, m, p):
+    dic = defaultdict()
+    dic = [for value in]
+    
+    answer = ''
+    return answer
+
+
+print(solution(2, 4, 2, 1))  # "0111"
+# print(solution(16, 16, 2, 1))  # "02468ACE11111111"
+# print(solution(16, 16, 2, 2))  # "13579BDF01234567"
