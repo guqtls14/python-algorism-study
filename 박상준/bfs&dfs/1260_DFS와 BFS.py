0dfbac773837e3badb218d864ad3aba6913464d6
jihye94 <qkrtkdwns3410@naver.com>
2022-09-06 22:52:02 +0900

    구현

--- 박상준/bfs&dfs/1260_DFS와 BFS.py
+++ 박상준/bfs&dfs/1260_DFS와 BFS.py
@@ -9,22 +9,24 @@
  * -----------------------------------------------------------
  * 2022-09-06        qkrtkdwns3410       최초 생성
  """
+visited = []
 
-def bfs():
+def bfs(graph, start, visited):
     pass
 
-def dfs():
+def dfs(graph, start, visited):
     pass
 
 def solution(n, m, v, node):
     visited = [0] * (n + 1)
+    
     graph = [[] for _ in range(n + 1)]
     for node in node:
         graph[node[0]].append(node[1])
         graph[node[1]].append(node[0])
     print(f"graph : {graph}")
     
-    bfs(graph, 1)
-    dfs(graph, 1)
+    # bfs(graph, 1, visited)
+    dfs(graph, 1, visited)
 
 solution(n=4, m=5, v=1, node=[[1, 2], [1, 3], [1, 4], [2, 4], [3, 4]])
