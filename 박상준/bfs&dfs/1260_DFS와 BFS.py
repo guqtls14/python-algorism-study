17cb587283aabb223bfe5904590c6e31dccfc47b
jihye94 <qkrtkdwns3410@naver.com>
2022-09-07 18:25:59 +0900

    구현

--- 박상준/bfs&dfs/1260_DFS와 BFS.py
+++ 박상준/bfs&dfs/1260_DFS와 BFS.py
@@ -9,7 +9,9 @@
  * -----------------------------------------------------------
  * 2022-09-06        qkrtkdwns3410       최초 생성
  """
-visited = []
+import sys
+
+input = sys.stdin.readline
 
 def bfs(graph, start, visited):
     pass
@@ -17,16 +19,14 @@ def bfs(graph, start, visited):
 def dfs(graph, start, visited):
     pass
 
+# n 정점의 개수 m 간선의 개수 탐색을 시작할 정점의 번호 v node : 간선이 연결하는 두 정점의 번호
 def solution(n, m, v, node):
-    visited = [0] * (n + 1)
-    
-    graph = [[] for _ in range(n + 1)]
-    for node in node:
-        graph[node[0]].append(node[1])
-        graph[node[1]].append(node[0])
-    print(f"graph : {graph}")
-    
-    # bfs(graph, 1, visited)
-    dfs(graph, 1, visited)
+    graph = [[0] * (n + 1) for _ in range(n + 1)]
+    visited_list1 = [0] * (n + 1)
+    visitied_list2 = [0] * (n + 1)
+    for _ in range(m):
+        a, b = map(int, input().split())
+        graph[a][b] = graph[b][a] = 1
+    pass
 
 solution(n=4, m=5, v=1, node=[[1, 2], [1, 3], [1, 4], [2, 4], [3, 4]])
