adb98472b730c4b0deae73d82a97fb5779da3958
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-09-09 20:42:11 +0900

    구현

--- 박상준/문자열/1764.듣보잡.py
+++ 박상준/문자열/1764.듣보잡.py
@@ -9,8 +9,22 @@
  * -----------------------------------------------------------
  * 2022-09-09        ipeac       최초 생성
  """
+
 # 듣도 못한 사람 n, 보도 못한 사람 m
 n, m = map(int, input().split())
+
 # n개줄 걸쳐 듣도 못한 사람 이름
+no_listen = [input() for _ in range(n)]
 
 # n+2 줄부터 보도 못한 사람의 이름
+no_see = [input() for _ in range(m)]
+
+# set
+no_listen = set(no_listen)
+no_see = set(no_see)
+
+# 교집합
+intersections = sorted(list(no_listen.intersection(no_see)))
+print(len(intersections))
+for i in intersections:
+    print(i)
