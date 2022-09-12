6225a37b2be42d3432d9c4a71efa4f2e8e7e984f
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-09-12 23:09:46 +0900

    구현

--- 박상준/프로그래머스/행렬의 곱셈.py
+++ 박상준/프로그래머스/행렬의 곱셈.py
@@ -10,23 +10,12 @@
  * 2022-09-12        qkrtkdwns3410       최초 생성
  """
 
-def solution(arr1, arr2):
-    answer = []
-    # 0 1 2
-    for i in range(len(arr1)):
-        tmp_arr = []
-        # 0 1 2
-        for j in range(len(arr1[0])):
-            tmp = 0
-            for k in range(len(arr2[0])):
-                print("==========================================")
-                print(f"arr1 : {arr1[i][k]}")
-                print(f"arr1 : {arr2[k][j]}")
-                tmp += arr1[i][k] * arr2[k][j]
-            print(f"tmp : {tmp}")
-            tmp_arr.append(tmp)
-        answer.append(list(tmp_arr))
+def product_matrix(X, Y):
+    answer = [[sum(a * b for a, b in zip(X_row, Y_col)) for Y_col in zip(*Y)] for X_row in X]
     return answer
+
+def solution(arr1, arr2):
+    return product_matrix(arr1, arr2)
 
 # solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]])
 solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]], [[5, 4, 3], [2, 4, 1], [3, 1, 1]])
