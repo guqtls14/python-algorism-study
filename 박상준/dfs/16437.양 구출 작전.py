e8145397acdfea5dcbebb0b6a6a2da5d7bbee6b7
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-09-20 23:56:46 +0900

    구현

---
+++ 박상준/dfs/16437.양 구출 작전.py
@@ -0,0 +1,17 @@
+"""
+ *packageName    : 
+ * fileName       : 16437.양 구출 작전
+ * author         : ipeac
+ * date           : 2022-09-20
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-09-20        ipeac       최초 생성
+ """
+n = 4
+island = []
+for i in range(n - 1):
+    wolf_sheep, counts, bridge = input(), int(input()), int(input()),
+    
+    pass
