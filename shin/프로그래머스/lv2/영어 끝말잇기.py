d2cabdb09ce797aff4164295935ecb81eaaf35f7
kyle-shk <guqtls85@naver.com>
2022-10-04 00:49:31 +0900

    1004 hash & lv2 2문제

---
+++ shin/프로그래머스/lv2/영어 끝말잇기.py
@@ -0,0 +1,9 @@
+# def solution(s):
+#     count=0
+#     order=1
+
+#     for i in s:
+#         for j in i:
+
+
+# word=["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]
