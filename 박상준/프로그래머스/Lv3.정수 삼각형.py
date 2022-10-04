0118c3725c95013246b03628bc1ca4c2beae4414
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-10-04 19:29:19 +0900

    구현

---
+++ 박상준/프로그래머스/Lv3.정수 삼각형.py
@@ -0,0 +1,17 @@
+"""
+ *packageName    : 
+ * fileName       : Lv3.정수 삼각형
+ * author         : ipeac
+ * date           : 2022-10-04
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-10-04        ipeac       최초 생성
+ """
+
+def solution(triangle):
+    answer = 0
+    return answer
+
+print(solution())
